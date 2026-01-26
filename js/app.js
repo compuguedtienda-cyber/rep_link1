99aa6e054ba977615cb4181c2cac93e4780a1716
compuguedtienda-cyber <compuguedtienda@gmail.com>
2026-01-25 20:52:21 -0500

    www
    www

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let flipped=false;
+const card=document.getElementById('card');
+const btn=document.getElementById('btnMap');
+
+function toggleCard(){
+    flipped=!flipped;
+    card.classList.toggle('flip');
+    btn.textContent=flipped?'VOLVER':'VER MAPA';
+}
+
+btn.onclick=toggleCard;
+card.onclick=toggleCard;
+
+/* ===== TOUCH SWIPE ===== */
+let startX=0;
+card.addEventListener('touchstart',e=>{
+    startX=e.touches[0].clientX;
+});
+
+card.addEventListener('touchend',e=>{
+    let endX=e.changedTouches[0].clientX;
+    if(Math.abs(startX-endX)>50){
+        toggleCard();
+    }
+});
